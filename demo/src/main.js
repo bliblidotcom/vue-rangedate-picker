038354d839eb46412c29c95e2d3ec5b681514ba5
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-12-08 19:30:35 +0700

    :tv: add demo custom format

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,29 +2,15 @@ var app = new Vue({
   el: '#app',
   data () {
     return {
-      date1: {
-        start: '',
-        end: ''
-      },
-      date2: {
-        start: '',
-        end: ''
-      },
-      date3: {
+      selectedDate: {
         start: '',
         end: ''
       }
     }
   },
   methods: {
-    date1Selected: function (daterange) {
-      this.date1 = daterange
-    },
-    date2Selected: function (daterange) {
-      this.date2 = daterange
-    },
-    date3Selected: function (daterange) {
-      this.date3 = daterange
+    onDateSelected: function (daterange) {
+      this.selectedDate = daterange
     }
   },
   components: {
