bliblidotcom/vue-rangedate-picker

commit 77cf9836f96d389dc8ea1da41cb84fe627e0d085

fix broken page (remove unnecessary init range on created) and resolve conflict https://github.com/bliblidotcom/vue-rangedate-picker/pull/35

src/js/rangedate-picker.js
@@ -188,7 +188,6 @@ export default {
     }
   },
   created () {
-    this.dateRange = this.initRange || null
     if (this.isCompact) {
       this.isOpen = true
     }
@@ -270,7 +269,7 @@ export default {
       // bound by > 0 and < last day of month
       return result > 0 && result <= endMonthDate ? result : '&nbsp;'
     },
-    getNewDateRange (result, activeMonth) {
+    getNewDateRange (result, activeMonth, activeYear) {
       const newData = {}
       let key = 'start'
       if (!this.isFirstChoice) {
@@ -278,7 +277,7 @@ export default {
       } else {
         newData['end'] = null
       }
-      const resultDate = new Date(this.activeYearStart, activeMonth, result)
+      const resultDate = new Date(activeYear, activeMonth, result)
       if (!this.isFirstChoice && resultDate < this.dateRange.start) {
         this.isFirstChoice = false
         return { start: resultDate }
@@ -291,7 +290,8 @@ export default {
     },
     selectFirstItem (r, i) {
       const result = this.getDayIndexInMonth(r, i, this.startMonthDay) + 1
-      this.dateRange = Object.assign({}, this.dateRange, this.getNewDateRange(result, this.activeMonthStart))
+      this.dateRange = Object.assign({}, this.dateRange, this.getNewDateRange(result, this.activeMonthStart,
+      this.activeYearStart))
       if (this.dateRange.start && this.dateRange.end) {
         this.presetActive = ''
         if (this.isCompact) {
@@ -301,7 +301,8 @@ export default {
     },
     selectSecondItem (r, i) {
       const result = this.getDayIndexInMonth(r, i, this.startNextMonthDay) + 1
-      this.dateRange = Object.assign({}, this.dateRange, this.getNewDateRange(result, this.startNextActiveMonth))
+      this.dateRange = Object.assign({}, this.dateRange, this.getNewDateRange(result, this.startNextActiveMonth,
+      this.activeYearEnd))
       if (this.dateRange.start && this.dateRange.end) {
         this.presetActive = ''
       }

commit 30286aed6f155ef872d7fbb04f3fab430e977221

Update rangedate-picker.js
Setting an initial dateRange was removed/broken in commit 77cf983

src/js/rangedate-picker.js
@@ -188,6 +188,9 @@ export default {
     }
   },
   created () {
+    if (this.initRange) {
+      this.dateRange = this.initRange
+    }
     if (this.isCompact) {
       this.isOpen = true
     }